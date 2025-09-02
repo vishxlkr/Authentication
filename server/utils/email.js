18a0245cf878ed69ba935e96b137af73b3589c45
vishxlkr <vishxlkr@gmail.com>
2025-09-02 20:33:52 +0530

    update

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,18 +1,19 @@
 import nodemailer from "nodemailer";
 
+// Use environment variables
 const sendEmail = async (options) => {
    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      host: process.env.EMAIL_HOST, // from .env
+      port: process.env.EMAIL_PORT, // from .env
+      secure: false, // true for 465, false for other ports like 587
       auth: {
-         user: "vishxlkr@gmail.com",
-         pass: "gwjr jwdn xfwp yrrp",
+         user: process.env.EMAIL_USER, // from .env
+         pass: process.env.EMAIL_PASS, // from .env
       },
    });
 
    const mailOptions = {
-      from: "Verify <vishxlkr@gmail.com>",
+      from: `Verify <${process.env.EMAIL_USER}>`,
       to: options.email,
       subject: options.subject,
       text: options.message,
