f1e98f650d670d34f8fca4c60b134d919d6184e6
vishxlkr <vishxlkr@gmail.com>
2025-09-02 20:35:26 +0530

    update

--- server/utils/email.js
+++ server/utils/email.js
@@ -3,17 +3,17 @@ import nodemailer from "nodemailer";
 // Use environment variables
 const sendEmail = async (options) => {
    const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST, // from .env
-      port: process.env.EMAIL_PORT, // from .env
+      host: process.env.EMAIL_HOST, //gmail
+      port: process.env.EMAIL_PORT, //587
       secure: false, // true for 465, false for other ports like 587
       auth: {
-         user: process.env.EMAIL_USER, // from .env
-         pass: process.env.EMAIL_PASS, // from .env
+         user: process.env.EMAIL_USER, // your mail whose app password is created
+         pass: process.env.EMAIL_PASS, // app password
       },
    });
 
    const mailOptions = {
-      from: `Verify <${process.env.EMAIL_USER}>`,
+      from: `E-mail Verification <${process.env.EMAIL_USER}>`, // your mail your mail whose app password is created from which you want to send mail
       to: options.email,
       subject: options.subject,
       text: options.message,
